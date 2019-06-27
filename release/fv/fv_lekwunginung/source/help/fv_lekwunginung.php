2ec71ccfa7e250eefefca64af0a0b5f3d7e7d8ab
kcliff-tranaptic <kclifford@tranaptic.ca>
2019-06-27 14:22:52 -0700

    Added readme.htm, help php, and kps files for fv_lekwunginung.

---
+++ release/fv/fv_lekwunginung/source/help/fv_lekwunginung.php
@@ -0,0 +1,45 @@
+<?php 
+  $pagename = 'lək̓ʷəŋiʔnəŋ Keyboard Help';
+  $pagetitle = 'lək̓ʷəŋiʔnəŋ Keyboard Help';
+  $pagestyle = <<<END
+  
+#wikitext table { margin: 12px; }
+#wikitext table td { border: solid 1px #888888 !important }
+#wikitext table th { border: solid 1px #888888 !important; background: #ffcccc }
+.lang2 {font-family:"Aboriginal Sans"}  
+END;
+
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the <b>lək̓ʷəŋiʔnəŋ</b> language of the BC Coast region of Canada.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+
+<h2>Notes on touch layout</h2>
+<div id='wikitext'>
+<div class='vspace'></div>
+<h3>Main keyboard layout</h3>
+<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center' colspan='2'>q</td><td  align='center' colspan='2'>w ʷ</td><td  align='center' colspan='2'>e ə θ ᶱ</td><td  align='center' colspan='2'>r</td><td  align='center' colspan='2'>t</td><td  align='center' colspan='2'>y ƛ̓</td><td  align='center' colspan='2'>u</td><td  align='center' colspan='2'>i</td><td  align='center' colspan='2'>o</td><td  align='center'>p</td></tr>
+<tr ><td  align='center' colspan='2'>a</td><td  align='center' colspan='2'>s š</td><td  align='center' colspan='2'>d</td><td  align='center' colspan='2'>f</td><td  align='center' colspan='2'>g</td><td  align='center' colspan='2'>h</td><td  align='center' colspan='2'>j</td><td  align='center' colspan='2'>k</td><td  align='center' colspan='2'>l ɫ</td><td  align='center'>&nbsp;</td></tr>
+<tr ><td >&nbsp;</td><td  align='center' colspan='2'>z</td><td  align='center' colspan='2'>x x̌</td><td  align='center' colspan='2'>c č</td><td  align='center' colspan='2'>v</td><td  align='center' colspan='2'>b</td><td  align='center' colspan='2'>n ŋ</td><td  align='center' colspan='2'>m</td><td  align='center' colspan='2'>&nbsp;</td><td  colspan='2'>&nbsp;</td></tr>
+</table>
+<br />
+<h3>Punctuation layout (hold-select ".")</h3>
+<table border='1' style='border-collapse:collapse' cellpadding='5' ><tr ><td  align='center' colspan='2'>.</td><td  align='center' colspan='2'>,</td><td  align='center' colspan='2'>!</td><td  align='center' colspan='2'>?</td><td  align='center' colspan='2'>'</td><td  align='center' colspan='2'>"</td></tr>
+<tr ><td  align='center' colspan='2'>/</td><td  align='center' colspan='2'>:</td><td  align='center' colspan='2'>;</td><td  align='center' colspan='2'>ʔ</td><th  align='center' colspan='2'> ́</th><th  align='center' colspan='2'> ̓</th></tr>
+</table>
+<div class='vspace'></div><ul><li>keys with a red background are combining accents.
+</li><li>the first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.
+</li></ul><div class='vspace'></div><h3>Alternate Name</h3>
+<ul><li>Songhees
+</li></ul>
+</div>
+
+<br/>
+
+<h2>Keyboard Layout</h2>
+<div id='osk'>
+</div>
